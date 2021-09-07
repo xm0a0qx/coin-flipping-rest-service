14eed44a6be51f3251fb9abe0174261724ff7b72
xm0a0qx <aleks.hal@pm.me>
2021-09-07 19:33:19 +0300

    COIN: initial commit

---
+++ src/main/java/com/sample/coinflippingrestservice/CoinFlippingRestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sample.coinflippingrestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinFlippingRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinFlippingRestServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sample/coinflippingrestservice/CoinFlippingRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.coinflippingrestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinFlippingRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
